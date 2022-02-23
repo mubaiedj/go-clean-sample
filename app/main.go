2fea998812f458935079713465c3c18880965177
JMubaied <mubaied.jm@gmail.com>
2022-02-23 09:28:54 -0300

    feature(main): Added gracefulShutdown process to main thread

--- app/main.go
+++ app/main.go
@@ -2,29 +2,48 @@ package main
 
 import (
 	"github.com/mubaiedj/go-clean-sample/app/application/process_order"
-	"github.com/mubaiedj/go-clean-sample/app/infrastructure/cockroach"
+	"github.com/mubaiedj/go-clean-sample/app/infrastructure/memory_database"
 	"github.com/mubaiedj/go-clean-sample/app/interfaces/web"
 	"github.com/mubaiedj/go-clean-sample/app/shared/utils/config"
 	"github.com/mubaiedj/go-clean-sample/app/shared/utils/log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
+
+const tenSecondRule = 10 * time.Second
 
 func main() {
 	log.Info("Preparing app configurations")
 	config.LoadSettings("test", "go-clean-sample", "config.yaml")
 
-	// Database connection
-	connectionCockroach := cockroach.CreateCockroachDbConnection()
-	cockroach.AutoMigrateEntities(connectionCockroach)
-
 	//Repository
-	//orderRepository := memory_database.NewOrdersRepository() // On Memory Repository
-	orderRepository := cockroach.NewOrdersRepository(connectionCockroach) // SQL Repository
+	orderRepository := memory_database.NewOrdersRepository() // On Memory Repository
+
+	// SQL Repository - TODO: PLease uncomment following lines if you want to inject a different database
+	//connectionCockroach := cockroach.CreateCockroachDbConnection()
+	//cockroach.AutoMigrateEntities(connectionCockroach)
+	//orderRepository := cockroach.NewOrdersRepository(connectionCockroach)
 
 	//UseCase
 	orderUseCase := process_order.NewOrderUseCase(orderRepository)
 
+	//Signs Catcher
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	//WebServer
 	web.NewWebServer()
 	web.InitRoutes(orderUseCase)
-	web.Start(config.GetString("web.port"))
+	go web.Start(config.GetString("web.port"))
+
+	//Graceful Shutdown process
+	sig := <-quit
+	gracefulShutdown(sig, web.Shutdown)
+}
+
+func gracefulShutdown(sig os.Signal, shutdownWebServer func()) {
+	shutdownWebServer()
+	log.Info("Shutdown process completed for signal: %v", sig)
 }
